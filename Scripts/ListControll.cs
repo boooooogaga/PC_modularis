b50550ff89862d76e6d1d9aaca683782468b7274
boo_ga_ga_ <vasyapetya233@gmail.com>
2025-02-26 16:35:25 +0200

    0.0.2
    - Optimized code

--- Scripts/ListControll.cs
+++ Scripts/ListControll.cs
@@ -14,95 +14,15 @@ public class ListControll : MonoBehaviour
     public TMP_Dropdown dropdownCase;
     public TMP_Dropdown dropdownRAM;
     public TMP_Dropdown dropdownCoolSystem;
-
+    
     public void Start()
     {
-        dropdownMother.onValueChanged.AddListener(MotherControl);
-        dropdownCPU.onValueChanged.AddListener(CPUControl);
-        dropdownGPU.onValueChanged.AddListener(GPUControl);
-        dropdownRAM.onValueChanged.AddListener(RAMControl);
-        dropdownCase.onValueChanged.AddListener(CaseControl);
-        dropdownCoolSystem.onValueChanged.AddListener(CoolControl);
-    }
-    public void MotherControl(int Value)
-    {
-        
-        if (Value == 1)
-        {
-            MotherCheck("ASUS ROG Strix Z490-E");
-        }
-        if (Value == 2)
-        {
-            MotherCheck("MSI MPG B550 Gaming Edge");
-        }
-    }
-    public void CPUControl(int Value) 
-    {
-        if (Value == 1)
-        {
-            CPUCheck("Intel Core i9-11900K");
-        }
-        if (Value == 2)
-        {
-            CPUCheck("AMD Ryzen 9 5900X");
-        }
-    }
-
-    public void GPUControl(int Value) 
-    {
-        if (Value == 1)
-        {
-            GPUCheck("NVIDIA GeForce RTX 3080");
-        }
-        if (Value == 2)
-        {
-            GPUCheck("AMD Radeon RX 6800 XT");
-        }
-        if (Value == 3)
-        {
-            GPUCheck("NVIDIA GeForce GTX 1660");
-        }
-        if (Value == 4)
-        {
-            GPUCheck("AMD Radeon RX 5700");
-        }
-        if (Value == 5)
-        {
-            GPUCheck("NVIDIA GeForce RTX 3070");
-        }
-    }
-    public void RAMControl(int Value) 
-    {
-        if (Value == 1)
-        {
-            RAMCheck("Corsair Vengeance LPX");
-        }
-        if (Value == 2)
-        {
-            RAMCheck("G.Skill Trident Z RGB");
-        }
-    }
-    public void CaseControl(int Value) 
-    {
-        if (Value == 1)
-        {
-            CaseCheck("NZXT H510");
-        }
-        if (Value == 2)
-        {
-            CaseCheck("Corsair 4000D");
-        }
-    }
-    public void CoolControl(int Value) 
-    {
-        if (Value == 1)
-        {
-            RAMCheck("Noctua NH-D15");
-        }
-        if (Value == 2)
-        {
-            RAMCheck("Corsair H100i RGB Platinum");
-        }
+        dropdownMother.onValueChanged.AddListener(index => MotherCheck(dropdownMother.options[index].text));
+        dropdownCPU.onValueChanged.AddListener(index => CPUCheck(dropdownCPU.options[index].text));
+        dropdownGPU.onValueChanged.AddListener(index => GPUCheck(dropdownGPU.options[index].text));
+        dropdownRAM.onValueChanged.AddListener(index => RAMCheck(dropdownRAM.options[index].text));
+        dropdownCase.onValueChanged.AddListener(index => CaseCheck(dropdownCase.options[index].text));
+        dropdownCoolSystem.onValueChanged.AddListener(index => CoolCheck(dropdownCoolSystem.options[index].text));
     }
     public void MotherCheck(string Motherboard)
     {
