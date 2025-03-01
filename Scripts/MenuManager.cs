1cee66a296078ddd74f1c4f27d2c8b5c4b869b85
zxcurist <tisenkosernij@gmail.com>
2025-03-01 09:04:47 +0200

    0.0.2
    MENU

---
+++ Scripts/MenuManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    /*public GameObject[] windows;
+    public void WindowsActive(string GoTo)
+    {
+        for (int i = 0; i < windows.Length; i++)
+        {
+            if (windows[i].name == GoTo)
+            {
+                windows[i].SetActive(true);
+            }
+            else
+            {
+                windows[i].SetActive(false);
+            }
+        }
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    public void GoToGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }*/
+}
